105fd8c620f20716d376277535bda3809d6225c2
Angelo Zambrano <angelozam17@gmail.com>
2023-06-05 17:35:45 -0500

    :feat: improve prompt

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use clap::Parser;
 use colored::Colorize;
 use sayme::{client, utils::format_text};
 use spinners;
+use std::env;
 
 #[derive(Debug, Parser)]
 #[clap(name = "sayme", version = "0.1.0", author = "Angelo Zambrano")]
 struct Cli {
-    prompt: String,
+    prompt: Vec<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let mut spinner = spinners::Spinner::new(spinners::Spinners::Dots9, "Loading...".into());
-    let response = client::make_request(cli.prompt.to_string()).send();
+    let mut spinner = spinners::Spinner::new(spinners::Spinners::Hamburger, "Loading...".into());
+    let prompt = build_prompt(cli.prompt.join(" ").as_str());
+    let response = client::make_request(prompt).send();
 
     let response = match response {
         Ok(response) => response,
@@ -43,4 +45,21 @@ fn main() {
     );
 
     spinner.stop_and_persist("✔".green().to_string().as_str(), answer);
+}
+
+fn build_prompt(prompt: &str) -> String {
+    let mut prompt = prompt.to_string();
+    let os = env::consts::OS;
+
+    if os == "windows" {
+        prompt.push_str(" (on Windows)");
+    } else if os == "linux" {
+        prompt.push_str(" (on Linux)");
+    } else if os == "macos" {
+        prompt.push_str(" (on macOS)");
+    }
+
+    prompt.push_str(":\n```bash\n#!/bin/bash\n");
+
+    prompt
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
 pub fn format_text(s: String) -> String {
-    let formatted_string = s.as_str().trim().replace("\n", "").to_string();
+    let formatted_string = s.as_str().trim().to_string();
     formatted_string
 }
